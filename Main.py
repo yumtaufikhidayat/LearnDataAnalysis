91c8a8a85f924d0a0918e2f405221cdea866d96c
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-10-11 10:52:16 +0700

    Feat: Adjust data type of order_date and delivery_date to date_time. Also adjust code to add a new line.

--- Main.py
+++ Main.py
@@ -12,94 +12,94 @@ display_width = pd.set_option('display.width', 1000)  # Set the width large enou
 # Load All Data
 customers_df = pd.read_csv("https://raw.githubusercontent.com/dicodingacademy/dicoding_dataset/main/DicodingCollection/customers.csv")
 # print(customers_df.head())
-# print("\n")
+# print()
 
 orders_df = pd.read_csv("https://raw.githubusercontent.com/dicodingacademy/dicoding_dataset/main/DicodingCollection/orders.csv")
 # print(orders_df.head())
-# print("\n")
+# print()
 
 product_df = pd.read_csv("https://raw.githubusercontent.com/dicodingacademy/dicoding_dataset/main/DicodingCollection/products.csv")
 # print(product_df.head())
-# print("\n")
+# print()
 
 sales_df = pd.read_csv("https://raw.githubusercontent.com/dicodingacademy/dicoding_dataset/main/DicodingCollection/sales.csv")
 # print(sales_df.head())
-# print("\n")
+# print()
 
 # 2. Assessing Data
 # print(customers_df.info())
-# print("\n")
+# print()
 
 ## There is an anomaly number with sum of gender data,
 ## so it needs to be sum to know a number of missing values
 ## 1. Sum all of missing values
 # print(customers_df.isna().sum())
-# print("\n")
+# print()
 
 ## 2. Show all of summed duplicated data
 # print("Sum of duplicated data: ", customers_df.duplicated().sum())
-# print("\n")
+# print()
 
 ## 3. Summary of statistic parameter (mean, median, mode, etc.)
 # print(customers_df.describe())
-# print("\n")
+# print()
 
 ## 4. Assessing data orders_df
 # print(orders_df.info())
-# print("\n")
+# print()
 
 ## 5. Check duplicated data
 # print("Sum of duplicated data: ", orders_df.duplicated().sum())
 # print(orders_df.describe())
-# print("\n")
+# print()
 
 ## 6. Assessing data product_df
 # print(product_df.info())
-# print("\n")
+# print()
 
 ## 7. Check duplicated data
 # print("Sum of duplicated data: ", product_df.duplicated().sum())
 # print(product_df.describe())
-# print("\n")
+# print()
 
 ## 8. Assessing data sales_df
 # print(sales_df.info())
-# print("\n")
+# print()
 
 ## 9. Sum all of missing values
 # print(sales_df.isna().sum())
-# print("\n")
+# print()
 
 ## 10. Check duplicated data
 # print("Sum of duplicated data: ", sales_df.duplicated().sum())
 # print(sales_df.describe())
-# print("\n")
+# print()
 
 # 3. Cleaning Data
 ## 1. Cleaning Data customer_df
 ### 1. Remove duplicate data
 # print(customers_df.drop_duplicates(inplace=True))
-# print("\n")
+# print()
 
 ### 2. Check duplicate data
 # print("Sum of duplicate data: ", customers_df.duplicated().sum())
-# print("\n")
+# print()
 
 ### 3. Handle missing values
 ### a. We need to filter the missing value
 # print(customers_df[customers_df.gender.isna()])
-# print("\n")
+# print()
 
 ### b. Then, we can use imputation method (replace with another value) to fix the missing values
 ### Imputation method chosen because the data frame has so much important information
 ### We can identify the dominant value, then fill the dominant value to gender column
 # print(customers_df.gender.value_counts())
 # print(customers_df.fillna(value="Prefer not to say", inplace=True))
-# print("\n")
+# print()
 
 ### c. Identify the missing value
 # print(customers_df.isna().sum())
-# print("\n")
+# print()
 
 ### d. Handle inaccurate value
 # pd.set_option('display.max_columns', None)  # Show all columns
@@ -108,17 +108,26 @@ sales_df = pd.read_csv("https://raw.githubusercontent.com/dicodingacademy/dicodi
 # print(display_expand_frame)
 # print(display_width)
 # print(customers_df[customers_df.age == customers_df.age.max()])
-# print("\n")
+# print()
 
 ### e. Replace false age by using replace method
 # print(customers_df.age.replace(customers_df.age.max(), 70, inplace=True))
 # print(customers_df.age.replace(customers_df.age.max(), 50, inplace=True))
-# print("\n")
+# print()
 
 ### f. Ensure the age equals to max age
 # print(customers_df[customers_df.age == customers_df.age.max()])
-# print("\n")
+# print()
 
 ### g. Ensure no inaccurate value
 # print(customers_df.describe())
-# print("\n")
+# print()
+
+## 2. Cleaning Data orders_cf
+### Change wrong data type of order_date & delivery_date
+datetime_columns = ["order_date", "delivery_date"]
+for column in datetime_columns:
+    orders_df[column] = pd.to_datetime(orders_df[column])
+
+print(orders_df.info())
+print("")
