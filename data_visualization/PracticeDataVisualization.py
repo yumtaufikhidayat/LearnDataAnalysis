01465c8f28bee8fbd444367624d2e7999620efec
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-10-18 20:13:08 +0700

    Feat: Add class to practice data visualization.

--- data_visualization/PracticeDataVisualization.py
+++ data_visualization/PracticeDataVisualization.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-
-display_max_column = pd.set_option('display.max_columns', None)    # Show all columns
+display_max_column = pd.set_option('display.max_columns', None)  # Show all columns
 display_expand_frame = pd.set_option('display.expand_frame_repr', False)  # Prevent DataFrame from breaking into multiple lines
 display_width = pd.set_option('display.width', 1000)  # Set the width large enough for DataFrame
 
@@ -315,3 +314,201 @@ print(all_df.groupby(by=["age_group", "product_type"]).agg({
 }))
 print()
 ##### ---- End of Explanatory Data Analysis ---- #####
+
+##### ---- Data Visualization ---- #####
+# 6. Data Visualization
+## 1. Show selling performance and revenue of corporation for the last month
+monthly_orders_df = all_df.resample(rule='ME', on='order_date').agg({
+    "order_id": "nunique",
+    "total_price": "sum"
+})
+
+monthly_orders_df.index = monthly_orders_df.index.strftime('%Y-%m')
+monthly_orders_df = monthly_orders_df.reset_index()
+monthly_orders_df.rename(columns={
+    "order_id": "order_count",
+    "total_price": "revenue"
+}, inplace=True)
+print(monthly_orders_df.head())
+print()
+
+### 1.Show line chart for order number in month
+monthly_orders_df = all_df.resample(rule='M', on='order_date').agg({
+    "order_id": "nunique",
+    "total_price": "sum"
+})
+monthly_orders_df.index = monthly_orders_df.index.strftime('%B')  # mengubah format order date menjadi nama bulan
+
+monthly_orders_df = monthly_orders_df.reset_index()
+monthly_orders_df.rename(columns={
+    "order_id": "order_count",
+    "total_price": "revenue"
+}, inplace=True)
+
+plt.figure(figsize=(10, 5))
+plt.plot(monthly_orders_df["order_date"], monthly_orders_df["order_count"], marker='o', linewidth=2, color="#72BCD4")
+plt.title("Number of Orders per Month (2021)", loc="center", fontsize=20)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+print(plt.show())
+print()
+
+### 2. Validation of total revenue
+plt.figure(figsize=(10, 5))
+plt.plot(
+    monthly_orders_df["order_date"],
+    monthly_orders_df["revenue"],
+    marker='o',
+    linewidth=2,
+    color="#72BCD4"
+)
+plt.title("Total Revenue per Month in 2021 (AUD)", loc="center", fontsize=20)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+print(plt.show())
+print()
+
+## 2. Product sell the most and the least
+sum_order_items_df = all_df.groupby("product_name").quantity_x.sum().sort_values(ascending=False).reset_index()
+print(sum_order_items_df.head(15))
+print()
+
+### 1. Show data using bar chart
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 6))
+colors = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(x="quantity_x", y="product_name", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel(None)
+ax[0].set_title("Best Performing Product", loc="center", fontsize=15)
+ax[0].tick_params(axis='y', labelsize=12)
+
+sns.barplot(x="quantity_x", y="product_name",
+            data=sum_order_items_df.sort_values(by="quantity_x", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel(None)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Worst Performing Product", loc="center", fontsize=15)
+ax[1].tick_params(axis='y', labelsize=12)
+
+plt.suptitle("Best and Worst Performing Product by Number of Sales", fontsize=20)
+print(plt.show())
+print()
+
+## 3. Demography of customer
+### 1. By gender
+bygender_df = all_df.groupby(by="gender").customer_id.nunique().reset_index()
+bygender_df.rename(columns={
+    "customer_id": "customer_count"
+}, inplace=True)
+
+plt.figure(figsize=(10, 5))
+
+sns.barplot(
+    y="customer_count",
+    x="gender",
+    data=bygender_df.sort_values(by="customer_count", ascending=False),
+    palette=colors
+)
+plt.title("Number of Customer by Gender", loc="center", fontsize=15)
+plt.ylabel(None)
+plt.xlabel(None)
+plt.tick_params(axis='x', labelsize=12)
+print(plt.show())
+print()
+
+### 2. By age
+byage_df = all_df.groupby(by="age_group").customer_id.nunique().reset_index()
+byage_df.rename(columns={
+    "customer_id": "customer_count"
+}, inplace=True)
+
+byage_df['age_group'] = pd.Categorical(byage_df['age_group'], ["Youth", "Adults", "Seniors"])
+plt.figure(figsize=(10, 5))
+colors_ = ["#D3D3D3", "#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(
+    y="customer_count",
+    x="age_group",
+    data=byage_df.sort_values(by="age_group", ascending=False),
+    palette=colors_
+)
+plt.title("Number of Customer by Age", loc="center", fontsize=15)
+plt.ylabel(None)
+plt.xlabel(None)
+plt.tick_params(axis='x', labelsize=12)
+print(plt.show())
+print()
+
+### 3. By states
+bystate_df = all_df.groupby(by="state").customer_id.nunique().reset_index()
+bystate_df.rename(columns={
+    "customer_id": "customer_count"
+}, inplace=True)
+
+plt.figure(figsize=(10, 5))
+colors_ = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(
+    x="customer_count",
+    y="state",
+    data=bystate_df.sort_values(by="customer_count", ascending=False),
+    palette=colors_
+)
+plt.title("Number of Customer by States", loc="center", fontsize=15)
+plt.ylabel(None)
+plt.xlabel(None)
+plt.tick_params(axis='y', labelsize=12)
+print(plt.show())
+print()
+
+## 4. RFM Analysis
+rfm_df = all_df.groupby(by="customer_id", as_index=False).agg({
+    "order_date": "max",  # mengambil tanggal order terakhir
+    "order_id": "nunique",  # menghitung jumlah order
+    "total_price": "sum"  # menghitung jumlah revenue yang dihasilkan
+})
+rfm_df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
+
+### 1. calculate when the customer last made a transaction (day)
+rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
+recent_date = orders_df["order_date"].dt.date.max()
+rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
+
+rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
+print(rfm_df.head())
+print()
+
+### 2. Show in chart
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(30, 6))
+
+colors = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
+
+#### 1. For recency
+sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors,
+            ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel(None)
+ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
+ax[0].tick_params(axis='x', labelsize=15)
+
+#### 2. For frequency
+sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5),
+            palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel(None)
+ax[1].set_title("By Frequency", loc="center", fontsize=18)
+ax[1].tick_params(axis='x', labelsize=15)
+
+#### 3. For monetary
+sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5),
+            palette=colors, ax=ax[2])
+ax[2].set_ylabel(None)
+ax[2].set_xlabel(None)
+ax[2].set_title("By Monetary", loc="center", fontsize=18)
+ax[2].tick_params(axis='x', labelsize=15)
+
+plt.suptitle("Best Customer Based on RFM Parameters (customer_id)", fontsize=20)
+print(plt.show())
+print()
+##### ---- End of Data Visualization ---- #####
